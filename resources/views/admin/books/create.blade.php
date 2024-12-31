02aecf54d16715c466ede01b940ac421be8aae57
takezo <taketo45@gmail.com>
2024-12-31 14:55:34 +0000

    create.blade.php old()

--- resources/views/admin/books/create.blade.php
+++ resources/views/admin/books/create.blade.php
@@ -20,17 +20,17 @@
             <label>カテゴリ</label>
             <select name="category_id">
                 @foreach($categories as $category)
-                    <option value="{{$category->id}}">{{$category->title}}</option>
+                    <option value="{{$category->id}}" @selected($category->id == old('category_id'))>{{$category->title}}</option>
                 @endforeach
             </select>
         </div>
         <div>
             <label for="title">タイトル</label>
-           <input type="text" name="title" placeholder="タイトル" id="title">
+           <input type="text" name="title" placeholder="タイトル" id="title" value="{{old('title')}}">
         </div>
         <div>
             <label for="price">価格</label>
-            <input type="text" name="price" placeholder="価格" id="price">
+            <input type="text" name="price" placeholder="価格" id="price" value="{{old('price')}}">
         </div>
         <input type="submit" value="送信">
     </form>
