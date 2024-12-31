7a4a3fbae55fcf5c3d5100e4caefaa66dab8de03
takezo <taketo45@gmail.com>
2024-12-31 13:55:54 +0000

    CRUD plus View

---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\View\View;
+use App\Models\Book;
+use App\Models\Category;
+
+
+class BookController extends Controller
+{
+    public function index(): Collection
+    {
+        $books = Book::all();
+        return $books;
+    }
+
+    public function show(string $id): Book
+    {
+        $book = Book::findOrFail($id);
+        return $book;
+    }
+
+    public function create(): View
+    {
+        $categories = Category::all();
+
+        return view('admin.books.create', [
+            'categories' => $categories,
+        ]);
+    }
+
+    public function store(Request $request): Book
+    {
+        $book = new Book();
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->save();
+
+        return $book;
+    }
+}
