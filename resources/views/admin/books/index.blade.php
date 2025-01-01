9e8d2f847958cc607fd86f2d39209de26e344010
takezo <taketo45@gmail.com>
2025-01-01 14:07:58 +0000

    layout components impremented

--- resources/views/admin/books/index.blade.php
+++ resources/views/admin/books/index.blade.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>書籍一覧</title>
-</head>
-<body>
+<x-layouts.book-manager>
+    <x-slot:title>
+        書籍一覧
+    </x-slot:title>
     <h1>書籍一覧</h1>
     @if(session('message'))
         <div style="color:blue">
@@ -14,6 +10,4 @@
     @endif
     <a href="{{ route('book.create') }}">追加</a>
     <x-book-table :$books />
-
-</body>
-</html>
+</x-layouts.book-manager>
